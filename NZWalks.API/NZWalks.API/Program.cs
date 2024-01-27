ec0796c37d3879682d3391c533fb24a68bf4d0f7
dat-tnm <tnmdat183@gmail.com>
2024-01-27 12:50:31 +0700

    create new web API

--- NZWalks.API/NZWalks.API/Program.cs
+++ NZWalks.API/NZWalks.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<NZWalksDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalks"));
+});
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
